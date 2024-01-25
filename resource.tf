281d1ebe6500cbb0d7ce35c6712ff2eb2c3afecc
Shaik Latheef <143796689+latheef1403@users.noreply.github.com>
2024-01-26 00:29:25 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my-ec2-vm" {
   ami               = "ami-0a3c3a20c09d6f377"
   instance_type     = "t2.micro"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
 }
