55bdc7affba4a42cbf62fdc27e22afee8133dc1f
Shaik Latheef <143796689+latheef1403@users.noreply.github.com>
2024-01-26 00:11:46 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAVRUVUDOTPS5KR5MA"
-  secret_key = "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"
+  access_key = "AKIAVRUVUDOTALFMQHJO"
+  secret_key = "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"
 }
