180e2d436e0606623fab7b7e32a77a397377f42a
Shaik Latheef <143796689+latheef1403@users.noreply.github.com>
2024-01-26 10:38:34 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,5 +1,8 @@
-resource "aws_instance" "my-ec2-vm" {
-  ami               = "ami-0a3c3a20c09d6f377"
-  instance_type     = "t2.micro"
-  location = "us-east-1"
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-buckt788"
+
+  tags = {
+    Name        = "My bucke788"
+    Environment = "Dev"
+  }
 }
